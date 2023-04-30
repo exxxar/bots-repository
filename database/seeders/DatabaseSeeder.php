1b617d4202092ffc9a44a9733f7cb7b67dbcc72e
Alex <exxxar@gmail.com>
2023-04-30 23:08:34 +0300

    large update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
            // RoleSeeder::class,
            // UserSeeder::class,
-            //CompanySeeder::class,
+            CompanySeeder::class,
            // LocationSeeder::class,
             BotTypeSeeder::class,
             BotSeeder::class,
