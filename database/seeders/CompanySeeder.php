325f78c9c8d391e8d05f8562265eb0db518de9f9
Alex <exxxar@gmail.com>
2023-04-30 23:05:06 +0300

    large update

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -12,52 +12,57 @@ class CompanySeeder extends Seeder
      */
     public function run(): void
     {
-        Company::query()->create([
-            'title'=>"CashMan",
-            'slug'=>"cashman",
-            'description'=>"Добро пожаловать!Меня зовут CashMan. Я виртуальный помощник по созданию продающих Телеграмм-Ботов.",
-            'address'=>"бул. Шевченко, 13, Донецк",
-            'image'=>"d0451060e588ccb84087d.jpg",
-            'phones'=>[
-              "+7(949)432-06-01",
-              "+7(949)432-06-02",
-              "+7(949)432-06-03",
-            ],
-            'links'=>null,
-            'email'=>"inbox@your-cashman.ru",
-            'schedule'=>null,
-            'manager'=>"Егор",
-            'is_active'=>true,
-        ]);
+        $company = Company::query()
+            ->where("slug", "cashman")
+            ->first();
 
-     /*   Company::query()->create([
-            'title'=>"\"Delivery Rocket\" сервис доставки",
-            'slug'=>"delivery_rocket",
-            'description'=>"Добро пожаловать в систему управления доставкой Delivery Rocket от CashMan",
-            'address'=>"бул. Шевченко, 13, Донецк",
-            'phones'=>[
-                "+7(949)432-06-01",
-                "+7(949)432-06-02",
-                "+7(949)432-06-03",
-            ],
-            'image'=>"d0451060e588ccb84087d.jpg",
-            'links'=>[
-                "https://vk.com/delivery_rocket",
-                "https://t.me/delivery_rocket",
-                "https://instagram.com/delivery_rocket",
-            ],
-            'email'=>"delivery-rocket@your-cashman.ru",
-            'schedule'=>[
-                "ПН 10:00 - 20:00",
-                "ВТ 10:00 - 20:00",
-                "СР 10:00 - 20:00",
-                "ЧТ 10:00 - 20:00",
-                "ПТ 10:00 - 20:00",
-                "СБ 12:00 - 20:00",
-                "ВС 12:00 - 16:00",
-            ],
-            'manager'=>"Анатолий",
-            'is_active'=>true,
-        ]);*/
+        if (is_null($company))
+            Company::query()->create([
+                'title' => "CashMan",
+                'slug' => "cashman",
+                'description' => "Добро пожаловать!Меня зовут CashMan. Я виртуальный помощник по созданию продающих Телеграмм-Ботов.",
+                'address' => "бул. Шевченко, 13, Донецк",
+                'image' => "d0451060e588ccb84087d.jpg",
+                'phones' => [
+                    "+7(949)432-06-01",
+                    "+7(949)432-06-02",
+                    "+7(949)432-06-03",
+                ],
+                'links' => null,
+                'email' => "inbox@your-cashman.ru",
+                'schedule' => null,
+                'manager' => "Егор",
+                'is_active' => true,
+            ]);
+
+        /*   Company::query()->create([
+               'title'=>"\"Delivery Rocket\" сервис доставки",
+               'slug'=>"delivery_rocket",
+               'description'=>"Добро пожаловать в систему управления доставкой Delivery Rocket от CashMan",
+               'address'=>"бул. Шевченко, 13, Донецк",
+               'phones'=>[
+                   "+7(949)432-06-01",
+                   "+7(949)432-06-02",
+                   "+7(949)432-06-03",
+               ],
+               'image'=>"d0451060e588ccb84087d.jpg",
+               'links'=>[
+                   "https://vk.com/delivery_rocket",
+                   "https://t.me/delivery_rocket",
+                   "https://instagram.com/delivery_rocket",
+               ],
+               'email'=>"delivery-rocket@your-cashman.ru",
+               'schedule'=>[
+                   "ПН 10:00 - 20:00",
+                   "ВТ 10:00 - 20:00",
+                   "СР 10:00 - 20:00",
+                   "ЧТ 10:00 - 20:00",
+                   "ПТ 10:00 - 20:00",
+                   "СБ 12:00 - 20:00",
+                   "ВС 12:00 - 16:00",
+               ],
+               'manager'=>"Анатолий",
+               'is_active'=>true,
+           ]);*/
     }
 }
