fb62a9b0bd905be060547d19cddc1a46659aacb3
Alex <exxxar@gmail.com>
2023-07-07 00:01:31 +0300

    large update

--- app/Classes/BotCore.php
+++ app/Classes/BotCore.php
@@ -84,6 +84,9 @@ abstract class BotCore
                 $slugActualKeyCollection[] = $config->key;
             }
 
+            Log::info("slugActualKeyCollection=>".print_r($slugActualKeyCollection, true));
+            Log::info("classKeyCollection=>".print_r($classKeyCollection, true));
+
             $diff = array_diff($slugActualKeyCollection, $classKeyCollection);
 
             if (count($diff) > 0)
@@ -234,7 +237,7 @@ abstract class BotCore
                         $arguments[] = $match;
 
 
-                    //$this->selfScriptDiagnostic($template);
+                    $this->selfScriptDiagnostic($template);
 
                     $find = $this->tryCall($item, $message, $template->config ?? null, ...$arguments);
                     break;
@@ -284,7 +287,7 @@ abstract class BotCore
 
                         if (!is_null($item)) {
 
-                            //$this->selfScriptDiagnostic($slug);
+                            $this->selfScriptDiagnostic($slug);
 
                             $this->tryCall($item, $message,
                                 $slug->config ?? null, []);
