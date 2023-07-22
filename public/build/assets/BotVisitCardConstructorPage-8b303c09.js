1e7ffa407f32940c1b8fd500f69c9e345bd6da9d
Alex <exxxar@gmail.com>
2023-07-23 00:31:27 +0300

    large modules update

---
+++ public/build/assets/BotVisitCardConstructorPage-8b303c09.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./MainAdminLayout-88a469cd.js";import{V as a}from"./VisitCardConstructor-7ef2c801.js";import{o,f as r,l as s,b as e}from"./app-d543012c.js";const _={},u=Object.assign(_,{__name:"BotVisitCardConstructorPage",setup(c){return(i,n)=>(o(),r(t,{active:5},{default:s(()=>[e(a)]),_:1}))}});export{u as default};
