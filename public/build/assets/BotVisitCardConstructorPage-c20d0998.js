aa19c82b63d9108175f06e60b2f29b01402d7b1d
Alex <exxxar@gmail.com>
2023-07-26 00:59:33 +0300

    prepare admins route

---
+++ public/build/assets/BotVisitCardConstructorPage-c20d0998.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./MainAdminLayout-db4e9215.js";import{V as a}from"./VisitCardConstructor-22c8b010.js";import{o,f as r,l as s,b as e}from"./app-56c84f84.js";const _={},u=Object.assign(_,{__name:"BotVisitCardConstructorPage",setup(c){return(i,n)=>(o(),r(t,{active:5},{default:s(()=>[e(a)]),_:1}))}});export{u as default};
