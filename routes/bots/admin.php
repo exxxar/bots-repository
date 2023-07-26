cd21d84df1236372e9d3adae93d1c46075dc4c87
Alex <exxxar@gmail.com>
2023-07-26 17:57:54 +0300

    add admin menu to route

--- routes/bots/admin.php
+++ routes/bots/admin.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Bots\AdminBotController;
 
 BotManager::bot()
     ->controller(AdminBotController::class)
-    ->route("/admin-menu", "getBotAdminMenu")
+    ->route("/adminmenu", "getBotAdminMenu")
     ->slug("slug_news_1", "getNews")
     ->slug("slug_admin_menu_1", "getBotAdminMenu");
