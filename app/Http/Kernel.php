b08b25199ab21f96bbebb26123d682052e8c7654
Alex <exxxar@gmail.com>
2023-07-28 23:15:36 +0300

    test session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,7 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'tgAuth:any' => TelegramAuthCheck::class,
-        'tgAuth:admin' => TelegramAdminCheck::class,
+        'tgAuth.any' => TelegramAuthCheck::class,
+        'tgAuth.admin' => TelegramAdminCheck::class,
     ];
 }
