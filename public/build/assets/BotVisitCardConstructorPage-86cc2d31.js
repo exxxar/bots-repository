35e70c5a460ec3f0c29c33495ef94e7b4b14635a
Alex <exxxar@gmail.com>
2023-07-28 23:44:00 +0300

    test session

--- public/build/assets/BotVisitCardConstructorPage-86cc2d31.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./MainAdminLayout-b5b264ad.js";import{V as a}from"./VisitCardConstructor-4d547afc.js";import{o,f as r,l as s,b as e}from"./app-3d85007a.js";const _={},u=Object.assign(_,{__name:"BotVisitCardConstructorPage",setup(c){return(i,n)=>(o(),r(t,{active:5},{default:s(()=>[e(a)]),_:1}))}});export{u as default};
