6bb08dbaa9f46d7cac9e0fb89e1c354bb3b8367e
Alex <exxxar@gmail.com>
2023-09-10 02:27:05 +0300

    change shop

--- resources/js/ClientTg/Pages/Result.vue
+++ resources/js/ClientTg/Pages/Result.vue
@@ -5,31 +5,30 @@ defineProps({
     }
 });
 
-import CallbackForm from "@/ClientTg/Components/Shop/CallbackForm.vue";
+
 import ReturnToBot from "@/ClientTg/Components/Shop/Helpers/ReturnToBot.vue";
-import Layout from "@/ClientTg/Layouts/ShopLayout.vue";
 </script>
 <template>
 
-    <Layout>
+    <div class="d-flex justify-content-center align-items-center w-100" style="height: 100vh;">
+        <div class="card card-style bg-8 w-100 px-3" data-card-height="250" style="height: 250px;">
+            <div class="card-center">
+                <h2 class="color-white font-700 text-center mb-2">Результат</h2>
+                <p class="color-white boxed-text-l text-center opacity-60 mt-n1 mb-3">
+                    {{ message }}
+                </p>
 
-        <template #default>
-
-            <div class="card card-style bg-8" data-card-height="250" style="height: 250px;">
-                <div class="card-center">
-                    <h2 class="color-white font-700 text-center mb-2">Результат</h2>
-                    <p class="color-white boxed-text-l text-center opacity-60 mt-n1 mb-3">
-                        {{ message }}
-                    </p>
-
-                    <ReturnToBot/>
-                </div>
-                <div class="card-overlay bg-black opacity-80"></div>
+                <ReturnToBot/>
             </div>
+            <div class="card-overlay bg-black opacity-80"></div>
+        </div>
 
-            <CallbackForm/>
-        </template>
-    </Layout>
+    </div>
+
+
+
+
+
 </template>
 <script>
 import {mapGetters} from "vuex";
