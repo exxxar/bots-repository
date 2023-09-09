105dac5304ee9ad8d2b0d95aeda66f633041da8d
Alex <exxxar@gmail.com>
2023-09-10 02:59:58 +0300

    change shop

--- resources/views/exports/cashback.blade.php
+++ resources/views/exports/cashback.blade.php
@@ -30,20 +30,20 @@
 
         @foreach($history as $cashback)
             <tr>
-                <td style="width: 100px;">{{$cashback->bot->bot_domain?? '❌'}}</td>
-                <td style="width: 100px;">{{$cashback->user->botUser->name?? '❌'}}</td>
-                <td style="width: 100px;">{{$cashback->user->botUser->fio_from_telegram?? '❌'}}</td>
-                <td style="width: 100px;"> {{$cashback->user->botUser->phone?? '❌'}}</td>
-                <td style="width: 50px;"> {{$cashback->user->botUser->is_vip ? "✔":"❌"}}</td>
-                <td style="width: 50px;"> {{$cashback->user->botUser->is_admin ? "✔":"❌"}}</td>
+                <td style="width: 100px;">{{$cashback->bot->bot_domain?? '-'}}</td>
+                <td style="width: 100px;">{{$cashback->user->botUser->name?? '-'}}</td>
+                <td style="width: 100px;">{{$cashback->user->botUser->fio_from_telegram?? '-'}}</td>
+                <td style="width: 100px;"> {{$cashback->user->botUser->phone?? '-'}}</td>
+                <td style="width: 50px;"> {{$cashback->user->botUser->is_vip ? "+":"-"}}</td>
+                <td style="width: 50px;"> {{$cashback->user->botUser->is_admin ? "+":"-"}}</td>
 
-                <td style="width: 200px;"> {{$cashback->description?? '❌'}}</td>
-                <td style="width: 100px;"> {{$cashback->operation_type == 1? "✔":"❌"}}</td>
-                <td style="width: 100px;"> {{$cashback->money_in_check?? '❌'}}</td>
-                <td style="width: 100px;"> {{$cashback->amount?? '❌'}}</td>
-                <td style="width: 100px;"> {{$cashback->level?? '❌'}}</td>
-                <td style="width: 100px;">{{$cashback->employee->fio_from_telegram?? '❌'}}
-                    ({{$cashback->employee->name?? '❌'}})
+                <td style="width: 200px;"> {{$cashback->description?? '-'}}</td>
+                <td style="width: 100px;"> {{$cashback->operation_type == 1? "+++":"---"}}</td>
+                <td style="width: 100px;"> {{$cashback->money_in_check?? '-'}}</td>
+                <td style="width: 100px;"> {{$cashback->amount?? '-'}}</td>
+                <td style="width: 100px;"> {{$cashback->level?? '-'}}</td>
+                <td style="width: 100px;">{{$cashback->employee->fio_from_telegram?? '-'}}
+                    ({{$cashback->employee->name?? '-'}})
                 </td>
 
 
