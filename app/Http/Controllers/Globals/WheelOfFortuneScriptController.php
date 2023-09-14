26b74932cd8584bc4d489b2a59f944faa76e201c
Alex <exxxar@gmail.com>
2023-09-14 23:37:13 +0300

    update

--- app/Http/Controllers/Globals/WheelOfFortuneScriptController.php
+++ app/Http/Controllers/Globals/WheelOfFortuneScriptController.php
@@ -229,7 +229,7 @@ class WheelOfFortuneScriptController extends SlugController
 
         $action->data = $tmp;
 
-        $link = "https://t.me/$bot->bot_domain?start=" . bcrypt("003$botUser->telegram_chat_id");
+        $link = "https://t.me/$bot->bot_domain?start=" . base64_encode("003$botUser->telegram_chat_id");
 
         Log::info("link=$link");
 
