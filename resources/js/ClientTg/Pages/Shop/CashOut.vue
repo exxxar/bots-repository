e6ed7b923595bfa8d8615b55fe012ec38a5e43ca
Alex <exxxar@gmail.com>
2023-09-27 18:25:08 +0300

    добавил модуль вывода средств

--- resources/js/ClientTg/Pages/Shop/CashOut.vue
+++ resources/js/ClientTg/Pages/Shop/CashOut.vue
@@ -28,7 +28,7 @@ import ProjectInfoCard from "ClientTg@/Components/Shop/Helpers/ProjectInfoCard.v
                     <h6 class="text-center">Введите номер телефона или своей карты для вывода средств</h6>
                     <div class="input-style input-style-2">
                         <input type="text" class="form-control text-center font-14 p-3 rounded-s border-theme"
-                               v-mask="'+7(###)###-##-##|#### #### #### ####'"
+                               v-mask="['+7(###)###-##-##','#### #### #### ####']"
                                v-model="withDrawMoneyForm.card"
                                placeholder="+7(000)000-00-00 или 0000 0000 0000 0000"
                                aria-label="withDrawMoneyForm-card" aria-describedby="withDrawMoneyForm-card" required>
