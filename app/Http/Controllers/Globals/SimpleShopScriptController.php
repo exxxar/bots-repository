c8d3f65f5c7a06cb34f51d21aeaa979060f201c9
Alex <exxxar@gmail.com>
2023-10-14 22:09:17 +0300

    bug fixes

--- app/Http/Controllers/Globals/SimpleShopScriptController.php
+++ app/Http/Controllers/Globals/SimpleShopScriptController.php
@@ -254,13 +254,13 @@ class SimpleShopScriptController extends SlugController
 
         if ($page == 0)
             $keyboard[] = [
-                ["text" => "Следующий товар", "callback_data" => "/next_global_products " . ($page + 1). " " . ($categoryId ?? 0)],
+                ["text" => "Следующий товар", "callback_data" => "/next_global_products " . ($page + 1) . " " . ($categoryId ?? 0)],
             ];
 
         if ($page >= 1)
             $keyboard[] = [
                 ["text" => "⬅ " . ($page - 1) . "/$allProductCount", "callback_data" => "/next_global_products " . ($page - 1) . " " . ($categoryId ?? 0)],
-                ["text" => ($page + 1) . "/$allProductCount ➡", "callback_data" => "/next_global_products " . ($page + 1). " " . ($categoryId ?? 0)],
+                ["text" => ($page + 1) . "/$allProductCount ➡", "callback_data" => "/next_global_products " . ($page + 1) . " " . ($categoryId ?? 0)],
             ];
 
         if (!is_null($messageId)) {
@@ -424,8 +424,8 @@ class SimpleShopScriptController extends SlugController
     {
         $messageId = $data[0]->message_id ?? null;
         $page = $data[3] ?? 0;
-        $categoryId =$data[4] ?? null;
-        $this->productsPage($page, $messageId,$categoryId);
+        $categoryId = $data[4] ?? null;
+        $this->productsPage($page, $messageId, $categoryId == 0 ? null : $categoryId);
     }
 
     public function nextCategories(...$data)
