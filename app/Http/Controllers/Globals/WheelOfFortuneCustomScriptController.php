6329d4e7d525f477471477c8dfda41877c7221f2
Alex <exxxar@gmail.com>
2023-10-22 00:45:24 +0300

    update

--- app/Http/Controllers/Globals/WheelOfFortuneCustomScriptController.php
+++ app/Http/Controllers/Globals/WheelOfFortuneCustomScriptController.php
@@ -219,7 +219,7 @@ class WheelOfFortuneCustomScriptController extends SlugController
             ->pluck("value")
             ->toArray();
 
-        $description = $wheelText[$winNumber ] ?? 'Без описания';
+        $description = $wheelText[$winNumber + 1] ?? 'Без описания';
 
         $tmp[] = (object)[
             "name" => $winnerName,
