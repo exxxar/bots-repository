fdfb83b2e88dc0f03537096f1c24af003ec71392
Alex <exxxar@gmail.com>
2023-11-13 21:09:31 +0300

    fixes

--- app/Http/BusinessLogic/Methods/BotPageLogicFactory.php
+++ app/Http/BusinessLogic/Methods/BotPageLogicFactory.php
@@ -157,6 +157,8 @@ class BotPageLogicFactory
 
         $tmp->reply_keyboard_id = null;
         $tmp->inline_keyboard_id = null;
+        $tmp->videos = isset($data["videos"]) ? json_decode($data["videos"] ?? '[]') : null;
+
         $tmp->content = $tmp->content ?? "";
         $tmp->is_external = (bool)($tmp->is_external ?? false);
         $tmp->bot_id = $this->bot->id;
@@ -265,6 +267,7 @@ class BotPageLogicFactory
         $photos = $this->uploadPhotos("/public/companies/$company->slug", $uploadedPhotos);
 
         $tmp = (object)$pageData;
+        $tmp->videos = isset($data["videos"]) ? json_decode($data["videos"] ?? '[]') : null;
         $tmp->is_external = (bool)($tmp->is_external ?? false);
         $tmp->content = $tmp->content ?? "";
 
