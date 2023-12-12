b6859496af9e7d5483c84a4671b9b64a9b7a3ac4
Alex <exxxar@gmail.com>
2023-12-12 13:20:06 +0300

    shuffle wheel items

--- resources/js/ClientTg/Components/Other/WheelSecond.vue
+++ resources/js/ClientTg/Components/Other/WheelSecond.vue
@@ -22,8 +22,22 @@
 export default {
     props:["items"],
     mounted() {
-        // надписи и цвета на секторах
-        const prizes = this.items /*[
+
+        function shuffle(array) {
+            let currentIndex = array.length,  randomIndex;
+            // While there remain elements to shuffle.
+            while (currentIndex > 0) {
+                // Pick a remaining element.
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+                // And swap it with the current element.
+                [array[currentIndex], array[randomIndex]] = [
+                    array[randomIndex], array[currentIndex]];
+            }
+            return array;
+        }
+
+        const prizes =    shuffle(this.items); /*[
             {
                 text: "Скидка 10%",
                 color: "hsl(197 30% 43%)",
