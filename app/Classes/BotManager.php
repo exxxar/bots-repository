7cfc6f777d6cb9ae423059fefbc76ab5198adfbe
Alex <exxxar@gmail.com>
2023-12-14 19:44:00 +0300

    fix media too

--- app/Classes/BotManager.php
+++ app/Classes/BotManager.php
@@ -420,7 +420,7 @@ class BotManager extends BotCore
             try {
                 $this->replyMediaGroup($media);
 
-
+                $needSendReplyMenu = falseж
             } catch (\Exception $e) {
                 $this->replyPhoto("Ошибочка с изображениями",
                     InputFile::create(public_path() . "/images/cashman2.jpg")
