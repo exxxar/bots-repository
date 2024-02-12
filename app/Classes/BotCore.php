6ecb1118e607c64e689258efe37f537d97ba7837
Alex <exxxar@gmail.com>
2024-02-12 18:07:52 +0300

    shop fixes

--- app/Classes/BotCore.php
+++ app/Classes/BotCore.php
@@ -726,7 +726,7 @@ abstract class BotCore
         $update = $this->bot->getWebhookUpdate();
 
 
-        Log::info(print_r($update, true));
+        //Log::info(print_r($update, true));
 
         include_once base_path('routes/bot.php');
 
