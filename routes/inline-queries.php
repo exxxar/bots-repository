ebf127503f9713835d087506e56590a8b09db5ac
Alex <exxxar@gmail.com>
2024-02-12 20:48:16 +0300

    add inline quiery handler

--- routes/inline-queries.php
+++ routes/inline-queries.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Bots\InlineBotController;
 
 \App\Facades\InlineQueryService::inline()
     ->controller(InlineBotController::class)
-    ->query("", "baseMenu")
-    ->query("меню", "inlineHandler");
+    ->query("меню", "baseMenu")
+    ->query("админы", "inlineHandler");
