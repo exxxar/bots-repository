12ea8a87d47660483cdf1a5aa9ba2ec340a427ba
Alex <exxxar@gmail.com>
2024-02-26 22:55:19 +0300

    update

--- app/Http/Resources/PromoCodeResource.php
+++ app/Http/Resources/PromoCodeResource.php
@@ -20,7 +20,7 @@ class PromoCodeResource extends JsonResource
             'slot_amount' => $this->slot_amount,
             'cashback_amount' => $this->cashback_amount,
             'max_activation_count' => $this->max_activation_count,
-            'current_activation_count' => $this->whenLoaded('botUsers') ?? 0,
+            'current_activation_count' => count($this->whenLoaded('botUsers') ?? []),
             'is_active' => $this->is_active,
             //'botUsers' => BotUserCollection::make($this->whenLoaded('botUsers')),
         ];
