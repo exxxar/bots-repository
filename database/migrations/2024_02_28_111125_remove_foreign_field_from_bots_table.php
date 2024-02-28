336fc6ae15f286585414c62209b574b07e01c28b
Alex <exxxar@gmail.com>
2024-02-28 14:17:22 +0300

    change relations

--- database/migrations/2024_02_28_111125_remove_foreign_field_from_bots_table.php
+++ database/migrations/2024_02_28_111125_remove_foreign_field_from_bots_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::disableForeignKeyConstraints();
         Schema::table('bots', function (Blueprint $table) {
+
+            $table->dropForeign(['creator_id']);
+
             $table->foreignId('creator_id')
                 ->change()
                 ->nullable();
