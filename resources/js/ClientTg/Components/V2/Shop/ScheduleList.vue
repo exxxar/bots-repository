c2d569936ddf30f90f58dc7717f6e30ac9e40387
Alex <exxxar@gmail.com>
2024-08-18 13:06:10 +0300

    fix schedule

--- resources/js/ClientTg/Components/V2/Shop/ScheduleList.vue
+++ resources/js/ClientTg/Components/V2/Shop/ScheduleList.vue
@@ -4,16 +4,16 @@
         <li
             v-for="(item, index) in sorted"
             class="list-group-item d-flex justify-content-between"
-            v-bind:class="{'current-day text-primary fw-bold':(new Date()).getUTCDay()-1 === index}"
+            v-bind:class="{'current-day text-primary fw-bold':preparedDay == index}"
             aria-current="true">
-            <span>{{item.day || '-'}}</span>
+            <span>{{ item.day || '-' }}</span>
             <span
                 v-if="!item.closed"
-                class="text-primary fw-bold">{{item.start_at || '-'}} - {{item.end_at || '-'}}</span>
+                class="text-primary fw-bold">{{ item.start_at || '-' }} - {{ item.end_at || '-' }}</span>
 
             <span
                 v-if="item.closed"
-                class="text-primary fw-bold">{{item.closed_comment || '-'}}</span>
+                class="text-primary fw-bold">{{ item.closed_comment || '-' }}</span>
         </li>
 
 
@@ -21,18 +21,21 @@
 </template>
 <script>
 export default {
-    props:["schedule"],
-    computed:{
-        sorted(){
+    props: ["schedule"],
+    computed: {
+        preparedDay() {
+            let day = (new Date()).getUTCDay();
+
+            return day === 0 ? 6 : day - 1
+        },
+        sorted() {
             const map = {
-                'Понедельник': 1,'Вторник': 2,'Среда': 3,'Четверг': 4,'Пятница': 5,'Суббота': 6, 'Воскресенье': 7
+                'Понедельник': 1, 'Вторник': 2, 'Среда': 3, 'Четверг': 4, 'Пятница': 5, 'Суббота': 6, 'Воскресенье': 7
             };
 
-            let tmp = this.schedule.sort((a, b) => {
+            return this.schedule.sort((a, b) => {
                 return map[a.day] - map[b.day];
-            });
-
-            return tmp
+            })
         }
     }
 }
