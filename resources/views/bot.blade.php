c30e305babc1a6125db8b246598d7e42d23ce4e9
Alex <exxxar@gmail.com>
2024-09-06 23:55:19 +0300

    updat

--- resources/views/bot.blade.php
+++ resources/views/bot.blade.php
@@ -27,7 +27,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <link rel="stylesheet" id="theme" href="https://your-cashman.com/theme11.bootstrap.min.css">
+    <link rel="stylesheet" id="theme" href="https://your-cashman.com/theme6.bootstrap.min.css">
 </head>
 
 <style>
