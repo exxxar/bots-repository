253901d2b97cc65926f43d7e04dbdeb327e4344f
exxxar <exxxar@gmail.com>
2024-11-12 23:45:36 +0300

    add statistic to big admin

--- database/migrations/2024_11_12_174803_add_field_to_bot_pages_table.php
+++ database/migrations/2024_11_12_174803_add_field_to_bot_pages_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bot_pages', function (Blueprint $table) {
-            //
+            $table->double("cashback")->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('bot_pages', function (Blueprint $table) {
-            //
+            $table->dropColumn("cashback");
         });
     }
 };
