59e7d1cc9a2a3f3f025ad3ce8f26bbb7e257642b
exxxar <exxxar@gmail.com>
2025-02-04 22:16:41 +0300

    update

--- routes/codes.php
+++ routes/codes.php
@@ -7,11 +7,11 @@ StartCodesService::bot()
     ->regular("/\b([0-9]{3})U([0-9]{10})B([0-9]{10})A([0-9]{10})\b/", "paymentAction")
     ->regular("/\b([0-9]{3})([0-9]{8,10})S([0-9]+)\b/", "slugAction")
     ->regular("/\b([0-9]{3})([0-9]{8,10})O([0-9]+)\b/", "orderAction")
-    ->regular("/\b([0-9]{3})([0-9]+)\b/", "referralAction")
     ->regular("/\b([0-9]{3})PAGE([0-9]+)\b/", "editPage")
     ->regular("/\b([0-9]{3})slug([0-9]+)table([0-9]{1,2})\b/", "openTableMenu")
     ->regular("/\b([0-9]{3})register([0-9]+)\b/", "confirmRegistrationAndLogin")
-    ->regular("/\b([0-9]{3})([0-9]+)utm([a-zA-Z0-9]+)\b/", "runPageAction");
+    ->regular("/\b([0-9]{3})([0-9]+)utm([a-zA-Z0-9]+)\b/", "runPageAction")
+    ->regular("/\b([0-9]{3})([0-9]+)\b/", "referralAction");
 
 
 
