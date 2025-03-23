747264fbb3172372a9127e7b55efc6409e5b2516
exxxar <exxxar@gmail.com>
2025-03-23 23:34:02 +0300

    update

--- app/Http/BusinessLogic/Methods/CDEKLogicFactory.php
+++ app/Http/BusinessLogic/Methods/CDEKLogicFactory.php
@@ -537,7 +537,7 @@ class CDEKLogicFactory extends BaseLogicFactory
             'sender' => BaseTypes\Contact::create([
                 'company' => "Гараев Иван Александрович",//$this->bot->company->title ?? $this->bot->bot_domain ?? 'Интернет-магазин',
                 'name' => "Гараев Иван Александрович",$data["sender_name"] ?? 'CashMan',
-                'phones' =>[ BaseTypes\Phone::create(['number' => "+79263183806"])]// $s_phones,
+                'phones' =>$s_phones// ,
             ]),
             'recipient' => BaseTypes\Contact::create([
                 'name' => $data["recipient_name"],
