c5759cbf94fa7a0065371e2ea758c0ee235f7027
exxxar <exxxar@gmail.com>
2025-05-30 01:41:34 +0300

    update

---
+++ app/Console/Commands/CheckStories.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\BotMethods;
+use App\Facades\BotManager;
+use App\Models\Bot;
+use App\Models\BotUser;
+use App\Models\Story;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CheckStories extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bot:check-stories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'ежедневная проверка истечения историй';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        ini_set('max_execution_time', 30000);
+
+        $stories = Story::query()
+            ->get();
+
+        foreach ($stories as $story) {
+
+            $createdAt = Carbon::parse($story->created_at);
+            $now = Carbon::now();
+
+            if ($createdAt->lessThan($now) && $createdAt->diffInSeconds($now) > 172800) {
+               $story->delete();
+            }
+        }
+
+        ini_set('max_execution_time', 300);
+    }
+}
---
+++ app/Console/Commands/CheckTables.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\BotMethods;
+use App\Facades\BotManager;
+use App\Models\Bot;
+use App\Models\BotUser;
+use App\Models\Story;
+use App\Models\Table;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CheckTables extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bot:check-tables';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'ежедневная проверка истечения времени столика';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        ini_set('max_execution_time', 30000);
+
+        $tables = Table::query()
+            ->whereNotNull("closed_at")
+            ->get();
+
+        foreach ($tables as $table) {
+            $table->closed_at = Carbon::now();
+            $table->save();
+        }
+
+        ini_set('max_execution_time', 300);
+    }
+}
