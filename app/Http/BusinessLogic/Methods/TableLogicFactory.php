0510ca4380def419458f9c18899b12687b414ecb
exxxar <exxxar@gmail.com>
2025-05-30 19:17:12 +0300

    update

--- app/Http/BusinessLogic/Methods/TableLogicFactory.php
+++ app/Http/BusinessLogic/Methods/TableLogicFactory.php
@@ -295,7 +295,7 @@ class TableLogicFactory extends BaseLogicFactory
                 $query->where('officiant_id', $this->botUser->id)
                     ->orWhereNull("officiant_id");
             })
-            ->orderBy("number","asc")
+            ->orderBy("id","asc")
             ->paginate($size);
 
         return new TableCollection($tables);
