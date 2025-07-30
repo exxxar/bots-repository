72d6161e3a608988b89283499aae0aae8eb109ae
exxxar <exxxar@gmail.com>
2025-07-30 19:11:47 +0300

    update

--- resources/js/ClientTg/Components/V2/Shop/Cart/OfferForm.vue
+++ resources/js/ClientTg/Components/V2/Shop/Cart/OfferForm.vue
@@ -1,17 +1,33 @@
-<template v-if="bot.company.law_params?.offer_link">
-    <div class="alert alert-light my-2 border-primary">
-        <p class="mb-2">
-            Нажимая кнопку, вы соглашаетесь с условиями
-            <a :href="bot.company.law_params.offer_link" class="fw-bold" target="_blank">договора оферты</a>.
-        </p>
-        <div class="form-check form-switch">
+<template>
+    <template v-if="bot.company.law_params?.offer_link">
+        <div class="alert alert-light my-2 border-primary">
+            <p class="mb-2">
+                Нажимая кнопку, вы соглашаетесь с условиями
+                <a :href="bot.company.law_params?.offer_link" class="fw-bold" target="_blank">договора оферты</a>.
+            </p>
+            <div class="form-check form-switch">
 
-            <input
-                v-model="offer_agreement"
-                class="form-check-input" type="checkbox" role="switch" id="offerSwitch" checked>
-            <label class="form-check-label fw-bold" for="offerSwitch">Я соглашаюсь</label>
+                <input
+                    v-model="offer_agreement"
+                    class="form-check-input" type="checkbox" role="switch" id="offerSwitch" checked>
+                <label class="form-check-label fw-bold" for="offerSwitch">Я соглашаюсь</label>
+            </div>
         </div>
-    </div>
+    </template>
+    <template v-else>
+        <div class="alert alert-light my-2 border-primary">
+            <p class="mb-2">
+                Нажимая кнопку, вы соглашаетесь с условиями договора оферты данной компании.
+            </p>
+            <div class="form-check form-switch">
+
+                <input
+                    v-model="offer_agreement"
+                    class="form-check-input" type="checkbox" role="switch" id="offerSwitch" checked>
+                <label class="form-check-label fw-bold" for="offerSwitch">Я соглашаюсь</label>
+            </div>
+        </div>
+    </template>
 </template>
 <script>
 
